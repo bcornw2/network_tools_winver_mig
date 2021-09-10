024dc3e2041c08a55dd8c11e9a75ef78bc0ad786
Guy <33430452+bcornw2@users.noreply.github.com>
2021-09-10 10:32:19 -0400

    Add files via upload

---
+++ get_CSVMembership_n_requests_sanitized.ps1
@@ -0,0 +1,76 @@
+﻿###
+##
+##
+##
+##Make user friendly input read-host -read-input - "What is file name", use env. variable for home (~)
+
+$csv = import-CSV -path 'C:\Users\Benjamin Cornwell\temp\vdc_csv.csv'
+$reqs = import-CSV -path 'C:\Users\Benjamin Cornwell\temp\AAR_Requests.csv'
+Write-Host ""
+write-host "please wait..." -ForeGroundColor Yellow
+$groupNames = @("group1", "group2", "group3", "group4")
+try{
+    $membersLA = Get-ADGroupMember -Identity "group1" -Recursive | Get-ADUser -Properties Mail | Select-Object Mail
+    Write-Host "ABR Users group populated..."
+    $membersFI = Get-ADGroupMember -Identity "group2" -Recursive | Get-ADUser -Properties Mail | Select-Object Mail
+    Write-Host "Internet Access group populated..."
+    $membersRD = Get-ADGroupMember -Identity "group3" -Recursive | Get-ADUser -Properties Mail | Select-Object Mail
+    Write-Host "Remote Desktop Users group populated..."
+    $membersWE = Get-ADGroupMember -Identity "group4" -Recursive | Get-ADUser -Properties Mail | Select-Object Mail
+    Write-Host "USB write group populated..."
+    Write-Host "CHecking users... " -ForegroundColor Yellow
+}
+catch{ Write-Host "Proceeding..."}
+$reqsHash = @{}
+
+foreach($req in $reqs){
+    if($req."Access Level" -eq "Local (PC) Administrator"){
+        $a = "Local Admin, "
+    } else {$a = ""}
+
+    $reqsHash[$req."Created By" -replace "orgname_change\\",""] = $a+$req."Functionality Requested" -replace " – Full","" -Replace ",",", " -Replace "\[\]", ""
+    #$reqsHash
+}
+
+
+
+foreach($item in $csv)
+{
+    Write-Host "emp:  $($item.Email)"
+    $email = $($item.Email) -Replace " ", ""
+    
+    $email = Get-ADUser -Filter {EmailAddress -eq $email} -Properties EmailAddress | Select -ExpandProperty EmailAddress
+    
+    $name = Get-ADUser -Filter {EmailAddress -eq $email} -Properties EmailAddress | Select -ExpandProperty Name ##Select -ExpandProperty EmpID
+    $name -replace "orgname_change\",""
+
+    if($membersLA -match $email) {
+            $item.Local_Admin = "Y"
+            $item.Requests = $reqshash[$name]
+            Write-Host = "User: $email is in group1 group"
+        }else {$item.Local_Admin = ""}
+    
+    if($membersFI -match $email) {
+            $item.Full_Internet = "Y"
+            $item.Requests = $reqshash[$name]
+            Write-Host = "User: $email is in group2 group"
+        }else {$item.Full_Internet = ""}
+    
+    if($membersRD -match $email) {
+            $item.RDP_Users = "Y"
+            $item.Requests = $reqshash[$name]
+            Write-Host = "User: $email is in group3 group"
+        }else {$item.RDP_Users = ""}
+    
+    if($membersWE -match $email) {
+            $item.USB_Write = "Y"
+            $item.Requests = $reqshash[$name]
+            Write-Host = "User: $email is in group4 group"
+        }else {$item.USB_Write = ""}
+    
+
+
+    #Write-Host "RESULT: $item.$group" -ForegroundColor Cyan
+    
+}
+    $csv | Export-CSV -Path "C:\Users\Benjamin Cornwell\temp\results_vdc.csv"
---
+++ scripto2_sanitized.ps1
@@ -0,0 +1,55 @@
+﻿##AD Group Membership Report Checker
+##Please ensure you are connected to VPN, or on the M.C. Dean network before beginning. 
+
+##test connection
+Write-Host "Which AD Group to Audit?"
+Write-Host "1.) Systems / AD Administrators"
+Write-Host "2.) Database Administrators"
+Write-Host "3.) Network Administrators"
+Write-Host "4.) Network Monitoring"
+Write-Host "5.) Application Administrators"
+Write-Host "6.) Application Developers"
+Write-Host "7.) Local Admins"
+Write-host "8.) Full Internet Access"
+Write-host "9.) Remote Desktop Users"
+write-Host "0.) All Groups - RDP, ABR, Internet Full"
+$input = Read-Host "Which AD group would you like to report on?"
+
+
+    $groupNames
+
+switch($input) {
+    "1" {$groupName="group1"; $fileName="Systems_AD_Administrators"}
+    "2" {$groupName="group2"; $fileName="Database_Administrators"}
+    "3" {$groupName="group3"; $fileName="Network_Administrators"}
+    "4" {$groupName="group4"; $fileName="Network_Monitoring"}
+    "5" {$groupName="group5"; $fileName="Application_Administrators"}
+    "6" {$groupName=""; $fileName="Application_Administrators"}
+    "7" {$groupName="group6"; $fileName="local_admins $groupName"}
+    "8" {$groupName="group7"; $fileName="Full_Internet_Access"}
+    "9" {$groupName="group8"; $filename="Remote_Desktop_users"}
+    "0" {$groupNames="groupA", "groupB", "groupC", "groupD", "groupE", "groupF", "groupG", "groupH", "groupI"}
+    }
+
+    
+    # ===========================================
+
+    foreach ($groupName in $groupNames){
+
+        get-adgroupmember -identity $groupName -recursive | Where-Object { $_.ObjectClass -eq 'user' } -ErrorAction SilentlyContinue| Get-AdUser `
+    -Properties samaccountname, name, mail, EmployeeNumber, Department, Office, Title, Manager |Select SAMAccountName, Name, Title, Department, `
+    Office, @{name='ManagerName';expression={(Get-ADUser -Identity $_.manager | Select-Object -ExpandProperty `
+    name)}},@{name='ManagerEmailAddress';expression={(Get-ADUser -Identity $_.manager -Properties emailaddress `
+    | Select-Object -ExpandProperty emailaddress)}} | Export-CSV -Path "C:\Users\Benjamin Cornwell\temp\$groupName.csv" 
+    
+        $users = Get-ADGroupMember -Identity $groupName
+        $count = $users.count
+        Write-Host "User count for group: " -NoNewLine
+        Write-Host $groupName -ForegroundColor Yellow -NoNewLine
+        Write-Host ": $count" -ForegroundColor White
+        #if($input -eq "0"){
+          #  write-host "All user counts saved to self-named .csv files in C:\Users\Benjamin Cornwell\temp\" -ForegroundColor Cyan   
+      #  }
+        }
+
+        #Moniter Lizerd, or Monitor Lizord for cryptoAPI name.. written in python, probably, if there is no native java or C++ library.
